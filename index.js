59c48aa0e78b3a1691ff0a96f33f85e9eb4c6dc4
Jacob Delanyo Dotsey <dotseydelanyo11@gmail.com>
2024-10-31 15:39:03 +0000

    added the env file

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+
+
+// connect mongodb
+await mongoose.connect(process.env.MONGO_URI);
+
+//  create an express app
+const app = express();
+
+// use middlewares
+app.use(express.json());
+app.use(cors())
+
+
+
+
+//  listen for incoming requests
+const PORT = 3000
+app.listen(PORT, () => {
+    console.log(`app is listening on port: ${PORT}`);
+});
