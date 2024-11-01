25b053e925ab99c743ab823ead1fee149976f525
shellyb-Online <shellybanahene@gmail.com>
2024-11-01 09:18:18 +0000

    ticket user

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-
-
+import userRouter from "./routes/userroute.js";
 
 // connect mongodb
 await mongoose.connect(process.env.MONGO_URI);
@@ -12,9 +11,8 @@ const app = express();
 
 // use middlewares
 app.use(express.json());
-app.use(cors())
-
-
+app.use(cors());
+app.use(userRouter);
 
 
 //  listen for incoming requests
