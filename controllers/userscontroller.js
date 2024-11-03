b4a2cf33384a0a99eed560719b2655c0120619c4
Benedict Boadu-Boateng <bboaduboateng2000@gmail.com>
2024-11-03 20:01:31 +0000

    user connecting to tickets

--- controllers/userscontroller.js
+++ controllers/userscontroller.js
@@ -1,115 +1,113 @@
 import { UserModel } from "../models/usermodels.js";
-import { userLoginValidator, userSignUpValidator, userUpdateValidator } from "../validators/usersvalitor.js";
+import {
+  userLoginValidator,
+  userSignUpValidator,
+  userUpdateValidator,
+} from "../validators/usersvalitor.js";
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
 export const userSignup = async (req, res, next) => {
-    try {
-        // checking if inputed details are correct
-        const { error, value } = userSignUpValidator.validate(req.body);
-        if (error) {
-            res.status(422).json(error);
-        }
-        //  check the database if the email exist already
-        const user = await UserModel.findOne({ email: value.email });
-        if (user) {
-            res.status(200).json("user already exist");
-        }
-        const hashedpassword = bcrypt.hashSync(value.password, 10);
-        // save to database
-        await UserModel.create({
-            ...value,
-            password: hashedpassword
-        });
-        res.status(200).json("User Registered Successfully")
-    } catch (error) {
-        next(error);
+  try {
+    // checking if inputed details are correct
+    const { error, value } = userSignUpValidator.validate(req.body);
+    if (error) {
+      return res.status(422).json(error);
     }
-}
-
-export const userLogin = async (req, res, next) => {
-    try {
-        // validate the person credentials
-        const { error, value } = userLoginValidator.validate(req.body);
-        if (error) {
-            res.status(422).json(error);
-        }
-        // find user with email
-        const user = await UserModel.findOne({ email: value.email });
-        if (!user) {
-            res.status(404).json("Invalid Credentials");
-        }
-        // check for validity of password
-        const correctpassword = bcrypt.compare(value.password, user.password);
-        if (!correctpassword) {
-            res.status(404).json("Invalid Credentials");
-        }
-        // now generate a token for the person
-        const token = jwt.sign({ id: user.id }, process.env.JWT_PRIVATE_KEY, {
-            expiresIn: "24h",
-        });
-        res.json({
-            message: "user logged in", accessToken: token,
-        });
-    } catch (error) {
-        next(error);
+    //  check the database if the email exist already
+    const user = await UserModel.findOne({ email: value.email });
+    if (user) {
+      return res.status(200).json("user already exist");
     }
-}
-
-export const getProfile = async (req, res, next) => {
-    try {
-        const user = await UserModel
-            .findById(req.auth.id)
-            .select({
-                password: false,
-            });
-        res.status(200).json(user);
-    } catch (error) {
-        next(error);
-    }
-
-}
-
-export const updateUserProfile = async (req, res, next) => {
-    try {
-        // Validate the user request
-        const { error, value } = userUpdateValidator.validate(req.body);
-        if (error) {
-            return res.status(422).json({ error: error.details[0].message });
-        }
-        // Find the user ID
-        const updatedVendor = await UserModel.findByIdAndUpdate(
-            req.auth.id,
-            value,
-            { new: true }
-        );
-        if (!updatedVendor) {
-            return res.status(404).json({ message: "user not found" });
-        }
-        //Send the updated vendor profile
-        res.json("Profile updated successfully");
-    } catch (error) {
-        next(error);
-    }
-}
-
-
-export const deleteUser = async (req, res, next) => {
-    try {
-        // Find the user by ID
-        const user = await UserModel.findOneAndDelete(req.auth.id);
-
-        // If user is not found, send a 404 response
-        if (!user) {
-            return res.status(404).json({ message: "User not found" });
-        }
-
-        // Send success response
-        res.status(200).json({ message: "User deleted successfully" });
-    } catch (error) {
-        next(error);
-    }
+    const hashedpassword = bcrypt.hashSync(value.password, 10);
+    // save to database
+    await UserModel.create({
+      ...value,
+      password: hashedpassword,
+    });
+    return res.status(200).json("User Registered Successfully");
+  } catch (error) {
+    next(error);
+  }
 };
 
+export const userLogin = async (req, res, next) => {
+  try {
+    // validate the person credentials
+    const { error, value } = userLoginValidator.validate(req.body);
+    if (error) {
+      res.status(422).json(error);
+    }
+    // find user with email
+    const user = await UserModel.findOne({ email: value.email });
+    if (!user) {
+      res.status(404).json("Invalid Credentials");
+    }
+    // check for validity of password
+    const correctpassword = bcrypt.compare(value.password, user.password);
+    if (!correctpassword) {
+      res.status(404).json("Invalid Credentials");
+    }
+    // now generate a token for the person
+    const token = jwt.sign({ id: user.id }, process.env.JWT_PRIVATE_KEY, {
+      expiresIn: "24h",
+    });
+    res.json({
+      message: "user logged in",
+      accessToken: token,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
+export const getProfile = async (req, res, next) => {
+  try {
+    const user = await UserModel.findById(req.auth.id).select({
+      password: false,
+    });
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
 
+export const updateUserProfile = async (req, res, next) => {
+  try {
+    // Validate the user request
+    const { error, value } = userUpdateValidator.validate(req.body);
+    if (error) {
+      return res.status(422).json({ error: error.details[0].message });
+    }
+    // Find the user ID
+    const updatedVendor = await UserModel.findByIdAndUpdate(
+      req.auth.id,
+      value,
+      { new: true }
+    );
+    if (!updatedVendor) {
+      return res.status(404).json({ message: "user not found" });
+    }
+    //Send the updated vendor profile
+    res.json("Profile updated successfully");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  try {
+    // Find the user by ID
+    const user = await UserModel.findOneAndDelete(req.auth.id);
+
+    // If user is not found, send a 404 response
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Send success response
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
--- models/usermodels.js
+++ models/usermodels.js
@@ -2,21 +2,22 @@ import { Schema, model, Types } from "mongoose";
 import { toJSON } from "@reis/mongoose-to-json";
 
 const userSchema = new Schema(
-   {
-      name: { type: String, required: true },
-      email: { type: String, required: true, unique: true },
-      location: { type: String, required: true },
-      password: { type: String, required: true },
-      notifications: [
-         {
-            type: Types.ObjectId,
-            ref: 'Notification'
-         }
-      ]
-   },
-   {
-      timestamps: true
-   }
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    location: { type: String, required: true },
+    password: { type: String, required: true },
+    nationality: { type: String, required: true },
+    notifications: [
+      {
+        type: String,
+        // ref: 'Notification'
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
 );
 
 // Add the toJSON plugin
