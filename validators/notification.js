103a01503d59d24aa731217b8756fe5369a17037
Benedict Boadu-Boateng <bboaduboateng2000@gmail.com>
2024-11-03 16:57:27 +0000

    Davida Notification

--- validators/notification.js
+++ validators/notification.js
@@ -1,75 +1,75 @@
-import Joi from "joi";
+// import Joi from "joi";
 
-export const postNotificationValidator = Joi.object({
-    type: Joi.string()
-        .valid('Scheduled', 'Real-time')
-        .required()
-        .label('Notification Type'),
+// export const postNotificationValidator = Joi.object({
+//     type: Joi.string()
+//         .valid('Scheduled', 'Real-time')
+//         .required()
+//         .label('Notification Type'),
 
-    title: Joi.string()
-        .required()
-        .label('Notification Title'),
+//     title: Joi.string()
+//         .required()
+//         .label('Notification Title'),
 
-    message: Joi.string()
-        .required()
-        .label('Notification Message'),
+//     message: Joi.string()
+//         .required()
+//         .label('Notification Message'),
 
-    location: Joi.string()
-        .optional()
-        .label('Notification Location'),
+//     location: Joi.string()
+//         .optional()
+//         .label('Notification Location'),
 
-    userId: Joi.string()
-        .optional()
-        .label('User ID'),
+//     userId: Joi.string()
+//         .optional()
+//         .label('User ID'),
 
-    status: Joi.string()
-        .valid('Unread', 'Read')
-        .default('Unread')
-        .label('Notification Status'),
+//     status: Joi.string()
+//         .valid('Unread', 'Read')
+//         .default('Unread')
+//         .label('Notification Status'),
 
-    timestamp: Joi.date()
-        .default(() => Date.now())
-        .label('Timestamp'),
+//     timestamp: Joi.date()
+//         .default(() => Date.now())
+//         .label('Timestamp'),
 
-    sentVia: Joi.array()
-        .items(Joi.string().valid('Push', 'Email'))
-        .required()
-        .label('Sent Via')
-});
+//     sentVia: Joi.array()
+//         .items(Joi.string().valid('Push', 'Email'))
+//         .required()
+//         .label('Sent Via')
+// });
 
-export const updateNotificationValidator = Joi.object({
-    type: Joi.string()
-        .valid('Scheduled', 'Real-time')
-        .required()
-        .label('Notification Type'),
+// export const updateNotificationValidator = Joi.object({
+//     type: Joi.string()
+//         .valid('Scheduled', 'Real-time')
+//         .required()
+//         .label('Notification Type'),
 
-    title: Joi.string()
-        .required()
-        .label('Notification Title'),
+//     title: Joi.string()
+//         .required()
+//         .label('Notification Title'),
 
-    message: Joi.string()
-        .required()
-        .label('Notification Message'),
+//     message: Joi.string()
+//         .required()
+//         .label('Notification Message'),
 
-    location: Joi.string()
-        .optional()
-        .label('Notification Location'),
+//     location: Joi.string()
+//         .optional()
+//         .label('Notification Location'),
 
-    userId: Joi.string()
-        .optional()
-        .label('User ID'),
+//     userId: Joi.string()
+//         .optional()
+//         .label('User ID'),
 
-    status: Joi.string()
-        .valid('Unread', 'Read')
-        .default('Unread')
-        .label('Notification Status'),
+//     status: Joi.string()
+//         .valid('Unread', 'Read')
+//         .default('Unread')
+//         .label('Notification Status'),
 
-    timestamp: Joi.date()
-        .default(() => Date.now())
-        .label('Timestamp'),
+//     timestamp: Joi.date()
+//         .default(() => Date.now())
+//         .label('Timestamp'),
 
-    sentVia: Joi.array()
-        .items(Joi.string().valid('Push', 'Email'))
-        .required()
-        .label('Sent Via')
-});
+//     sentVia: Joi.array()
+//         .items(Joi.string().valid('Push', 'Email'))
+//         .required()
+//         .label('Sent Via')
+// });
--- validators/ticket.js
+++ validators/ticket.js
@@ -1,18 +1,15 @@
 import Joi from "joi";
 
 export const addTicketValidador = Joi.object({
-    date:Joi.string().required(),
-    department:Joi.string(). required(),
-    location:Joi.string().required(),
-    problem:Joi.string().required(),
-    description:Joi.string().required(),
-    photo:Joi.string(),
-    status:Joi.string()
-
-})
+  department: Joi.string().required(),
+  location: Joi.string().required(),
+  problem: Joi.string().required(),
+  description: Joi.string().required(),
+  photo: Joi.string(),
+  status: Joi.string(),
+});
 
 export const updateTicketValidator = Joi.object({
-    date:Joi.string().required(),
-    problem:Joi.string().required(),
-    description:Joi.string().required()
-})
+  problem: Joi.string().required(),
+  description: Joi.string().required(),
+});
