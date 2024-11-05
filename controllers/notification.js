c53cf470cc35e55741aa66c5533234250fbcefc0
Davida <byourself77by@gmail.com>
2024-11-05 16:19:24 +0000

    TICKET notification working

--- controllers/notification.js
+++
@@ -1,5 +0,0 @@
-// import { notificationModel } from "../models/notification.js"
-// import { TicketModel } from "../models/ticket.js";
-// import { postNotificationValidator} from "../validators/notification.js";
-
-//If an agent reads the ticket
